de1856e0af233bad3129874dff6f2c31ad1b3563
Nikk0o <nknk@lagartao.gay>
2026-04-20 12:08:08 -0300

    teste

--- default.nix
+++ default.nix
@@ -5,11 +5,14 @@
 
 		dontUnpack = true;
 
+		buildInputs = with pkgs; [ jekyll bundler ];
+		buildPhase = "jekyll build";
+
 		installPhase = ''
 			mkdir $out
-			mv ./* $out
+			mv ./_site $out
 
-			echo -e "#!bash\n ${pkgs.jekyll}/bin/jekyll serve -s $out --port 4000" > $out/run.sh
+			echo -e "#!bash\n${pkgs.jekyll}/bin/jekyll serve $out --port 4000 --skip-initial-build" > $out/run.sh
 			chmod +777 $out/run.sh
 		'';
 	}
