98408d5072747d21ce19264218c740c4693a43fb
Nikk0o <nknk@lagartao.gay>
2026-04-20 12:29:26 -0300

    Changed directory structure

--- default.nix
+++ default.nix
@@ -1,18 +1,15 @@
 { pkgs ? import <nixpkgs> {}, ... }:
 	pkgs.stdenv.mkDerivation {
 		name = "Niko personal website";
-		src = ./.;
+		src = ./src;
 
 		dontUnpack = true;
 
-		buildInputs = with pkgs; [ jekyll bundler ];
-		buildPhase = "jekyll build";
-
 		installPhase = ''
 			mkdir $out
-			mv ./_site $out
+			mv ./* $out
 
-			echo -e "#!bash\n${pkgs.jekyll}/bin/jekyll serve $out --port 4000 --skip-initial-build" > $out/run.sh
+			echo -e "#!bash\n${pkgs.jekyll}/bin/jekyll serve $out --port 4000" > $out/run.sh
 			chmod +777 $out/run.sh
 		'';
 	}
