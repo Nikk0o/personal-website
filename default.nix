dc945263b6908ae46491734c51fe85c612118320
Nikk0o <nknk@lagartao.gay>
2026-04-20 12:33:19 -0300

    ugh

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 { pkgs ? import <nixpkgs> {}, ... }:
 	pkgs.stdenv.mkDerivation {
 		name = "Niko personal website";
-		src = ./src;
+		src = ./.;
 
 		dontUnpack = true;
 
 		installPhase = ''
 			mkdir $out
-			mv ./* $out
+			mv ./src/* $out
 
 			echo -e "#!bash\n${pkgs.jekyll}/bin/jekyll serve $out --port 4000" > $out/run.sh
 			chmod +777 $out/run.sh
