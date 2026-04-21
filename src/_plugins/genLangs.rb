4d5cc07ea574241a2bdfc7961657465f6458dd90
Nikk0o <nknk@lagartao.gay>
2026-04-21 16:07:09 -0300

    Small changes in directory structure

--- src/_plugins/genLangs.rb
+++ src/_plugins/genLangs.rb
@@ -1,14 +1,14 @@
 module Langs
   class Generator < Jekyll::Generator
     def generate(site)
-      langs = site.data['langs']
-      siteMap = site.data['map']
+      langs = site.data['langs'].keys
+      siteMap = site.data['pages']
 
       langs.each do |lang|
         siteMap.each do |file|
-          newPage = Jekyll::Page.new(site, ".", ".", lang[0] + "-" + file['name'] + ".html")
-          newPage.data['lang'] = lang[0]
-          newPage.data['permalink'] = "/" + lang[0] + "/" + file['permalink']
+          newPage = Jekyll::Page.new(site, ".", ".", lang + "-" + file['name'] + ".html")
+          newPage.data['lang'] = lang
+          newPage.data['permalink'] = "/" + lang + "/" + file['permalink']
           newPage.data['layout'] = file['name']
           newPage.data['style'] = file['name'] + ".css"
 
--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-import { enableExit } from "./exitDropdown.js";
-
-window.onload = () => {
-	let exitButton = document.getElementById("exit-button"),
-		doorsList = document.getElementById("doors")
-
-	enableExit(exitButton, doorsList)
-}
