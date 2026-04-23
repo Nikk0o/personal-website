84665d792afd1f6eb7d6ec95886319fe670015f8
Nikk0o <nknk@lagartao.gay>
2026-04-23 14:23:08 -0300

    Updated how languages are displayes

--- src/assets/scripts/void.js
+++ src/assets/scripts/void.js
@@ -1,9 +1,7 @@
 import { enableSelectLang } from "./language.js"
 
-const lang = document.documentElement.lang
+const lang = document.documentElement.lang.split('-')[0]
 
 window.onload = () => {
 	enableSelectLang("lang-select-button", "lang-list")
-	document.getElementById(`lang-${lang}`).style.filter = "invert(20%);"
-	document.getElementById("current-lang").innerHTML = document.getElementById(`lang-${lang}`).innerHTML
 }
