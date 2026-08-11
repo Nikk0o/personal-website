28feb38b0ab79db5fa4f2cb0f2c953461130451c
Nikk0o <nknk@lagartao.gay>
2026-08-11 19:07:02 -0300

    Fix flake.nix

--- flake.nix
+++ flake.nix
@@ -6,12 +6,11 @@
 		flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils }: {
+  outputs = { self, nixpkgs, flake-utils }:
 		flake-utils.lib.eachDefaultSystem (system:
 			let pkgs = nixpkgs.legacyPackages.${system}; in
 			{
 				nixosModules.default = ./module.nix;
 			}
 		);
-  };
 }
