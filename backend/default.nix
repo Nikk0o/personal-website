375c3eed6ce86580eddff4299f289a909f8b4732
Nikk0o <nknk@lagartao.gay>
2026-08-12 10:30:11 -0300

    Update backend sha256

--- backend/default.nix
+++ backend/default.nix
@@ -3,6 +3,8 @@ pkgs.buildNpmPackage {
 	name = "lagarto-gay-backend";
 	src = ./.;
 
+	npmDepsHash = "sha256-rAopIuxC5ShPv3QvYAFgdeXBL+VMAdbx1WcMWAQculc=";
+
 	installPhase = ''
 		mkdir -p $out/backend
 		cp * $out/backend -r'';
