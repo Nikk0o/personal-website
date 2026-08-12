1c0318924ed9c4b8393344310ce471adb735363d
Nikk0o <nknk@lagartao.gay>
2026-08-12 10:44:32 -0300

    n aguento mais nada funciona

--- backend/default.nix
+++ backend/default.nix
@@ -3,7 +3,7 @@ pkgs.buildNpmPackage {
 	name = "lagarto-gay-backend";
 	src = ./.;
 
-	npmDepsHash = "sha256-rAopIuxC5ShPv3QvYAFgdeXBL+VMAdbx1WcMWAQculc=";
+	# npmDepsHash = "sha256-rAopIuxC5ShPv3QvYAFgdeXBL+VMAdbx1WcMWAQculc=";
 
 	dontNpmBuild = true;
 
