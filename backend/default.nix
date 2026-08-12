90c80bf67417fe08700af8adc1787ff83fb45248
Nikk0o <nknk@lagartao.gay>
2026-08-12 11:04:08 -0300

    mds

--- backend/default.nix
+++ backend/default.nix
@@ -5,8 +5,6 @@ pkgs.buildNpmPackage {
 
 	npmDepsHash = "sha256-lGFoCr749mnqWKG0vVvfP5R1ZjTc6/i0h5U69v9izB4=";
 
-	dontNpmBuild = true;
-
 	installPhase = ''
 		mkdir -p $out/backend
 		cp * $out/backend -r'';
